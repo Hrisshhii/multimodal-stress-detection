6cadaa4816820e6700fbc457bbc34e092091d44d
Hrishi <rathodhrishi12345@gmail.com>
2026-04-17 20:23:06 +0530

    Fixinging the ui for home screen

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,15 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
+    <main className="min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold">
          Multimodal stress detection
       </h1>
-      <Link href="/chat" className="ml-4 text-blue-500">Start Demo</Link>
+      <>
+        <Link href="/chat" className="ml-4 text-blue-500 p-2 mt-8
+         border border-white/50 rounded-xl">Start Demo</Link>
+      </>
+      
     </main>
   );
 }
